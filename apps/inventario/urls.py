93f21ff7875eeb0f4bccda44e14a34bf2f33a425
Rostan <allowed_g@hotmail.com>
2018-06-22 17:39:32 -0500

    Commit #8
    Cambios de look and feel, y buenas prácticas en el archivo urls.py
    
    apps\crm\...: Archivos nuevos por la creacion de la app de CRM.
    
    apps\inventario\urls.py: se implementan buenas practicas para llevar un orden de como proceden ciertas urls y sus views.
    
    templates\base\main_base.html: se descomenta el container, esto por darle un mejor look and feel al sitio.
    
    templates\inventario:
    - inv_adm_items.html: cambios por look and feel.
    - inv_items.html: cambios por look and feel.
    
    - db.sqlite3: Registros nuevos por pruebas.

--- apps/inventario/urls.py
+++ apps/inventario/urls.py
@@ -1,16 +1,21 @@
+from django.urls import include
 from django.urls import path, re_path
 from . import views
 
 app_name = 'inventario'
 
-urlpatterns = [
-    path('', views.inicio, name='inv_inicio'),
+urlmaestros = [
     re_path(r'^grupo/(?P<pk>[0-9a-zA-Z]+)/(?P<mode>[0-9a-zA-Z]+)/$', views.get_grupo, name='get_grupo'),
     re_path(r'^marca/(?P<pk>[0-9a-zA-Z]+)/(?P<mode>[0-9a-zA-Z]+)/$', views.get_marca, name='get_marca'),
     re_path(r'^item/(?P<pk>[0-9a-zA-Z ]+)/(?P<mode>[0-9a-zA-Z]+)/$', views.get_item, name='get_item'),
     re_path(r'^item_precio/(?P<pk>[0-9a-zA-Z ]+)/$', views.get_item, name='get_precio'),
     re_path(r'^item_set_precio/$', views.set_precio, name='itm_set_precio'),
-    re_path(r'^admin_items/$', views.ItemListView.as_view(), name='admin_items'),
+    re_path(r'^items/$', views.ItemListView.as_view(), name='admin_items'),
     re_path(r'^item_delete/$', views.delete_item, name='delete_item'),
     re_path(r'^filter_items/$', views.filter_items, name='filter_item'),
+]
+
+urlpatterns = [
+    path('', views.inicio, name='inv_inicio'),
+    path('admin/', include(urlmaestros)),
 ]
